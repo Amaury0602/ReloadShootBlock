ccbb91c9db7d540c3f719534e971b85433cde4fc
Amaury0602 <amauryropert@gmail.com>
2020-06-11 22:45:35 +0200

    Added timer sprites and code

---
+++ RSB/Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerActions : MonoBehaviour
+{
+    private Button[] actionButtons;
+    SelectedAction selectedAction;
+
+
+    void Start()
+    {
+        // lets get the 3 action buttons in an array and add listener so we can set callback
+        actionButtons = GetComponentsInChildren<Button>();
+        actionButtons[0].onClick.AddListener(delegate { SelectOnClick(0, SelectedAction.Reload); }); //buttonReload
+        actionButtons[1].onClick.AddListener(delegate { SelectOnClick(1, SelectedAction.Block); }); //button Block
+        actionButtons[2].onClick.AddListener(delegate { SelectOnClick(2, SelectedAction.Shoot); }); //button Shoot
+    }
+    
+
+    public void SelectOnClick(int index, SelectedAction action)
+    {
+        selectedAction = action;
+    }
+}
+
+public enum SelectedAction
+{
+    //by default, the block action is the selectedAction
+    Block, Shoot, Reload
+}
---
+++ RSB/Assets/TimerCircle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerCircle : MonoBehaviour
+{
+    [SerializeField] private float maximumTime;
+    private float actualTimer;
+    private Image fillCircle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualTimer = maximumTime;
+        fillCircle = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (actualTimer >= 0) actualTimer -= Time.deltaTime;
+        fillCircle.fillAmount = actualTimer / maximumTime;
+
+
+        if (actualTimer <= 0 && Input.GetKeyDown(KeyCode.P)) actualTimer = maximumTime;
+    }
+}
