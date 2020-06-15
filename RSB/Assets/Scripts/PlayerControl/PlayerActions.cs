a640e25dcc7464c725eb003f7670a635aced86d1
Amaury0602 <amauryropert@gmail.com>
2020-06-15 08:28:45 +0200

    added actionreceiver singleton, timerStop + id to playerActions

---
+++ RSB/Assets/Scripts/PlayerControl/PlayerActions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerActions : MonoBehaviour
+{
+    [SerializeField] private TimerCircle timer;
+    [SerializeField] public int id; // id lets you know which player sent the action
+    private Button[] actionButtons;
+    public SelectedAction ClickedAction { get; set; }
+
+
+    void Start()
+    {
+        // lets get the 3 action buttons in an array and add listener so we can set callback
+        actionButtons = GetComponentsInChildren<Button>();
+        actionButtons[0].onClick.AddListener(delegate { SendActionOnClick(0, SelectedAction.Reload, id); }); //buttonReload
+        actionButtons[1].onClick.AddListener(delegate { SendActionOnClick(1, SelectedAction.Block, id); }); //button Block
+        actionButtons[2].onClick.AddListener(delegate { SendActionOnClick(2, SelectedAction.Shoot, id); }); //button Shoot
+    }
+
+
+    //SelectedAction to send when player clicks on UI button
+    public SelectedAction SendActionOnClick(int index, SelectedAction action, int id)
+    {
+        timer.timerMustStop = true; // so the TimerCircle stops running
+        return action;
+        // send action to ActionReceiver.current with id so you trigger right image
+    }
+}
+
+
+
+
+public enum SelectedAction
+{
+    //by default, the block action is the selectedAction
+    Block, Shoot, Reload
+}
---
+++ RSB/Assets/Scripts/PlayerControl/TimerCircle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerCircle : MonoBehaviour
+{
+    [SerializeField] private float maximumTime;
+    [SerializeField] private int id;
+    public bool timerMustStop;
+    private float actualTimer;
+    private Image fillCircle;
+    [SerializeField] private PlayerActions playerAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerMustStop = false;
+        actualTimer = maximumTime;
+        fillCircle = transform.GetChild(0).GetComponent<Image>();
+        GameEvents.current.onTimerEndTrigger += OnTimerEnd;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (actualTimer >= 0 && !timerMustStop) actualTimer -= Time.deltaTime;
+        fillCircle.fillAmount = actualTimer / maximumTime;
+
+
+        if (actualTimer <= 0)
+        {
+            GameEvents.current.TimerEnd(id);
+            actualTimer = maximumTime; // the timer just loops so we can test it
+        }
+    }
+
+    private void OnTimerEnd(int id)
+    {
+        if (id == this.id) Debug.Log("the times endsssss"); // trigger something when timer ends
+        playerAction.SendActionOnClick(1, SelectedAction.Block, playerAction.id); // the default sent action is Block
+        //player cannot pickAction anymore
+    }
+
+    
+    private void OnDestroy()
+    {
+        GameEvents.current.onTimerEndTrigger -= OnTimerEnd;
+    }
+}
